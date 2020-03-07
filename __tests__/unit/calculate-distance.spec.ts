a97b52be8a0a3113050937a05e91b1e6c9bf9f5b
Gabriel de Freitas Pinheiro <gabrieldefreitaspinheiro@gmail.com>
2020-03-07 17:08:55 -0300

    Add the distance calculation tests

---
+++ __tests__/unit/calculate-distance.spec.ts
@@ -0,0 +1,21 @@
+import CalculateDistance, { Point } from "../../src/utils/calculate-distance";
+
+describe("Calculete Distance", () => {
+  it('should be able to calculate the distance between two points', async () => {
+    const point1: Point = { coordX: 0, coordY: 0 };
+    const point2: Point = { coordX: 3, coordY: 4 };
+    
+    const distance = CalculateDistance(point1, point2);
+
+    expect(distance).toBe(5);
+  });
+
+  it('should be able to calculate the distance between two points with posite result', async () => {
+    const point1: Point = { coordX: 3, coordY: 4 };
+    const point2: Point = { coordX: 0, coordY: 0 };
+    
+    const distance = CalculateDistance(point1, point2);
+
+    expect(distance).toBe(5);
+  });
+})
