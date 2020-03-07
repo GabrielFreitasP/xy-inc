3b881a40e185856c78cdf8e83d2ea6b09813a24d
Gabriel de Freitas Pinheiro <gabrieldefreitaspinheiro@gmail.com>
2020-03-07 02:34:31 -0300

    Configure the database connection

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,17 @@
 import 'dotenv/config';
+import mongoConfig from '@config/mongo';
+import mongoose from 'mongoose';
 import app from './app';
+
+mongoose.connect(
+  `mongodb://${mongoConfig.username}:${mongoConfig.password}@${mongoConfig.host}:${mongoConfig.port}/${mongoConfig.database}`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  },
+);
 
 app.listen(process.env.PORT || 3333, () => {
   console.log('⚡️ Server listening on http://localhost:3333');
