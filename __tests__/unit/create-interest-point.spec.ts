df867c1440bbf5f6074f0bba04927c0c1ad81f26
Gabriel de Freitas Pinheiro <gabrieldefreitaspinheiro@gmail.com>
2020-03-07 20:41:31 -0300

    Add interest point creating test

---
+++ __tests__/unit/create-interest-point.spec.ts
@@ -0,0 +1,38 @@
+import MongoMock from '../utils/MongoMock';
+import InterestPointData from '../../src/services/structures/InterestPointData';
+
+import CreateInterestPointService from '../../src/services/CreateInterestPointService';
+
+import InterestPoint from '../../src/schemas/InterestPoint';
+
+describe('Create Interest Point', () => {
+  beforeAll(async () => {
+    await MongoMock.connect();
+  });
+  
+  afterAll(async () => {
+    await MongoMock.disconnect();
+  });
+  
+  beforeEach(async () => {
+    await InterestPoint.deleteMany({});
+  });
+  
+  it('should be able to create new interest point', async () => {
+    const interestPoint: InterestPointData = { 
+      name: 'Lanchonete', 
+      coordinateX: 27, 
+      coordinateY: 12 
+    };
+
+    await CreateInterestPointService.run(interestPoint)
+
+    const list = await InterestPoint.find({});
+
+    expect(list).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining(interestPoint)
+      ])
+    );
+  });
+});
