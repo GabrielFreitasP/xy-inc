0967ae1e1e65891a181ed1c2d81ca0157b37b6a0
Gabriel de Freitas Pinheiro <gabrieldefreitaspinheiro@gmail.com>
2020-03-07 20:44:22 -0300

    Add interest point creating route test

---
+++ __tests__/integration/route-create-interest-point.spec.ts
@@ -0,0 +1,46 @@
+import MongoMock from '../utils/MongoMock';
+import request from 'supertest';
+
+import app from '../../src/app';
+import InterestPoint from '../../src/schemas/InterestPoint';
+
+describe('Route Create Interest Point', () => {
+  beforeAll(async () => {
+    await MongoMock.connect();
+  });
+  
+  afterAll(async () => {
+    await MongoMock.disconnect();
+  });
+  
+  beforeEach(async () => {
+    await InterestPoint.deleteMany({});
+  });
+  
+  it('should return status created', async () => {
+    const interestPoint = { 
+      name: 'Lanchonete', 
+      coordinateX: 27, 
+      coordinateY: 12 
+    };
+
+    const response = await request(app)
+      .post('/interestPoints')
+      .send(interestPoint);
+
+    expect(response.status).toBe(201);
+  });
+  
+  it('should return status bad request', async () => {
+    const interestPoint = { 
+      name: 'Lanchonete', 
+      coordinateX: 27
+    };
+
+    const response = await request(app)
+      .post('/interestPoints')
+      .send(interestPoint);
+
+    expect(response.status).toBe(400);
+  });
+});
