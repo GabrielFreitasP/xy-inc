92999ca13a33a603ef21a3ef3035a7d181c12757
Gabriel de Freitas Pinheiro <gabrieldefreitaspinheiro@gmail.com>
2020-03-08 15:10:41 -0300

    Add check number test on POI creating route test

--- __tests__/integration/route-create-poi.spec.ts
+++ __tests__/integration/route-create-poi.spec.ts
@@ -2,45 +2,59 @@ import MongoMock from '../utils/MongoMock';
 import request from 'supertest';
 
 import app from '../../src/app';
-import InterestPoint from '../../src/schemas/PointOfInterest';
+import PointOfInterest from '../../src/schemas/PointOfInterest';
 
 describe('Route to Create Point of Interest', () => {
   beforeAll(async () => {
     await MongoMock.connect();
   });
-  
+
   afterAll(async () => {
     await MongoMock.disconnect();
   });
-  
+
   beforeEach(async () => {
-    await InterestPoint.deleteMany({});
+    await PointOfInterest.deleteMany({});
   });
-  
+
   it('should return status created', async () => {
-    const interestPoint = { 
-      name: 'Lanchonete', 
-      coordinateX: 27, 
-      coordinateY: 12 
+    const poi = {
+      name: 'Lanchonete',
+      coordinateX: 27,
+      coordinateY: 0
     };
 
     const response = await request(app)
       .post('/pointsOfInterest')
-      .send(interestPoint);
+      .send(poi);
 
     expect(response.status).toBe(201);
   });
-  
+
   it('should return status bad request', async () => {
-    const interestPoint = { 
-      name: 'Lanchonete', 
+    const poi = {
+      name: 'Lanchonete',
       coordinateX: 27
     };
 
     const response = await request(app)
       .post('/pointsOfInterest')
-      .send(interestPoint);
+      .send(poi);
 
     expect(response.status).toBe(400);
+  });
+
+  it('should accept the number zero', async () => {
+    const poi = {
+      name: 'Lanchonete',
+      coordinateX: 0,
+      coordinateY: 0
+    };
+
+    const response = await request(app)
+      .post('/pointsOfInterest')
+      .send(poi);
+
+    expect(response.status).toBe(201);
   });
 });
