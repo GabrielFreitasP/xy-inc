15e81f46491caba725b752386aa2be4216c8c095
Gabriel de Freitas Pinheiro <gabrieldefreitaspinheiro@gmail.com>
2020-03-08 01:25:21 -0300

    Add interest points listing route

--- src/routes.ts
+++ src/routes.ts
@@ -1,18 +1,26 @@
 import { Router } from 'express';
+import InterestPointData from '@services/structures/InterestPointData';
 
 import CreateInterestPointService from '@services/CreateInterestPointService';
+import ListInterestPointsService from '@services/ListInterestPointsService';
 
 const routes = Router();
 
 routes.post('/interestPoints', async (req, res) => {
-  if (!req.body) return res.status(400).send({ status: 400, message: 'No body content' })
+  if (!req.body) return res.status(400).send({ success: false, message: 'No body content' })
 
   const { name, coordinateX, coordinateY } = req.body;
-  if (!name || !coordinateX || !coordinateY) return res.status(400).send({ status: 400, message: 'Invalid body content' })
+  if (!name || !coordinateX || !coordinateY) return res.status(400).send({ success: false, message: 'Invalid body content' })
   
-  const interestPoints = await CreateInterestPointService.run({ name, coordinateX, coordinateY });
+  const interestPoint = await CreateInterestPointService.run({ name, coordinateX, coordinateY });
 
-  return res.status(201).send({ status: 201, data: interestPoints });
+  return res.status(201).send({ success: true, data: interestPoint });
+});
+
+routes.get('/interestPoints', async (req, res) => {
+  let interestPointsArray = await ListInterestPointsService.run();
+
+  return res.status(200).send({ success: true, data: interestPointsArray });
 });
 
 export default routes;
