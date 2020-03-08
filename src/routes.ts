12350234b9ea7520b7420b898c54893a973e3891
Gabriel de Freitas Pinheiro <gabrieldefreitaspinheiro@gmail.com>
2020-03-08 17:25:45 -0300

    Add internal error message handling on routes

--- src/routes.ts
+++ src/routes.ts
@@ -8,43 +8,63 @@ import ListPOIsByProximityService from '@services/ListPOIsByProximityService';
 const routes = Router();
 
 routes.post('/pointsOfInterest', async (req, res) => {
-  if (!req.body) {
-    return res.status(400).send({
-      success: false,
-      message: 'No body content'
+  try {
+    if (!req.body) {
+      return res.status(400).send({
+        success: false,
+        message: 'No body content'
+      });
+    }
+
+    const { name, coordinateX, coordinateY } = req.body;
+    if (!name || !isValidNumber(coordinateX) || !isValidNumber(coordinateY)) {
+      return res.status(400).send({
+        success: false,
+        message: 'Invalid body content'
+      });
+    }
+
+    if (coordinateX < 0 || coordinateY < 0) {
+      return res.status(400).send({
+        success: false,
+        message: 'Negative values are\'t accepted'
+      });
+    }
+
+    const poi = await CreatePOIService.run({ name, coordinateX, coordinateY });
+
+    return res.status(201).send({
+      success: true,
+      data: poi
     });
   }
-
-  const { name, coordinateX, coordinateY } = req.body;
-  if (!name || !isValidNumber(coordinateX) || !isValidNumber(coordinateY)) {
-    return res.status(400).send({
+  catch (err) {
+    console.error(err);
+    
+    res.status(500).send({
       success: false,
-      message: 'Invalid body content'
+      message: 'Internal server error'
     });
   }
-
-  if (coordinateX < 0 || coordinateY < 0) {
-    return res.status(400).send({
-      success: false,
-      message: 'Negative values are\'t accepted'
-    });
-  }
-  
-  const poi = await CreatePOIService.run({ name, coordinateX, coordinateY });
-
-  return res.status(201).send({
-    success: true,
-    data: poi
-  });
 });
 
 routes.get('/pointsOfInterest', async (req, res) => {
-  const poisArray = await ListPOIsService.run();
+  try {
+    const poisArray = await ListPOIsService.run();
 
-  return res.status(200).send({
-    success: true,
-    data: poisArray
-  });
+    return res.status(200).send({
+      success: true,
+      data: poisArray
+    });
+  }
+  catch (err) {
+    console.error(err);
+    
+    res.status(500).send({
+      success: false,
+      message: 'Internal server error'
+    });
+  }
 });
 
 routes.get('/pointsOfInterest/byProximity', async (req, res) => {
@@ -55,34 +75,35 @@ routes.get('/pointsOfInterest/byProximity', async (req, res) => {
         message: 'No URL params (distance, coordinateX, coordinateY)'
       });
     }
-  
+
     const { distance, coordinateX, coordinateY } = req.query;
     if (!isValidNumber(distance) || !isValidNumber(coordinateY) || !isValidNumber(coordinateX)) {
-      return res.status(400).send({ 
-        success: false, 
-        message: 'Invalid URL params' 
+      return res.status(400).send({
+        success: false,
+        message: 'Invalid URL params'
       });
     }
-  
+
     if (distance < 0 || coordinateX < 0 || coordinateY < 0) {
-      return res.status(400).send({ 
-        success: false, 
-        message: 'Negative values are\'t accepted' 
+      return res.status(400).send({
+        success: false,
+        message: 'Negative values are\'t accepted'
       });
     }
-    
+
     const poisArray = await ListPOIsByProximityService.run({ distance, coordinateX, coordinateY })
-  
-    return res.status(200).send({ 
-      success: true, 
+
+    return res.status(200).send({
+      success: true,
       data: poisArray
-    }); 
-  } 
+    });
+  }
   catch (err) {
     console.error(err);
-    res.status(500).send({ 
-      success: false, 
-      message: 'Internal server error' 
+    
+    res.status(500).send({
+      success: false,
+      message: 'Internal server error'
     });
   }
 });
