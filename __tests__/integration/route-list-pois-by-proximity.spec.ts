9e11593d55ae8b99b022f78dddc70928cd84b46a
Gabriel de Freitas Pinheiro <gabrieldefreitaspinheiro@gmail.com>
2020-03-09 03:33:59 -0300

    Add query validations tests on test of route of POIs listing by proximity

--- __tests__/integration/route-list-pois-by-proximity.spec.ts
+++ __tests__/integration/route-list-pois-by-proximity.spec.ts
@@ -3,7 +3,6 @@ import request from 'supertest';
 import app from '../../src/app';
 
 import POI from '../../src/schemas/PointOfInterest';
-import POIData from '../../src/services/structures/PointOfInterestData';
 
 import { poisArrayMock } from '../utils/arrays-mock';
 
@@ -11,54 +10,69 @@ describe('Route to List Points of Interest by Proximity', () => {
   beforeAll(async () => {
     await MongoMock.connect();
   });
-  
+
   afterAll(async () => {
     await MongoMock.disconnect();
   });
-  
+
   beforeEach(async () => {
     await POI.deleteMany({});
   });
-  
-  it('should return status 200 with correct params', async () => {  
-    poisArrayMock.forEach(async (element: POIData) => {
-      await POI.create(element)
-    });
-    
+
+  it('should return status 200 with correct query', async () => {
+    await POI.create(poisArrayMock);
+
     const response = await request(app)
       .get('/pointsOfInterest/byProximity?distance=10&coordinateX=20&coordinateY=10');
-
-    const poisNearArray = [
-      poisArrayMock[0],
-      poisArrayMock[2],
-      poisArrayMock[4],
-      poisArrayMock[5]
-    ];
 
     expect(response.status).toBe(200);
     expect(response.body.success).toBe(true);
     expect(response.body.data).toBeInstanceOf(Array);
     expect(response.body.data.length).toBe(4);
     expect(response.body.data).toEqual(
-      expect.arrayContaining(poisNearArray)
+      expect.arrayContaining([
+        expect.objectContaining(poisArrayMock[0]),
+        expect.objectContaining(poisArrayMock[2]),
+        expect.objectContaining(poisArrayMock[4]),
+        expect.objectContaining(poisArrayMock[5])
+      ])
     );
   });
-  
-  it('should return status 400 missing params', async () => {
-    const response = await request(app)
-      .get('/pointsOfInterest/byProximity');    
 
-    expect(response.status).toBe(400);    
+  it('should return status 400 without query', async () => {
+    const response = await request(app)
+      .get('/pointsOfInterest/byProximity')
+      .send();
+
+    expect(response.status).toBe(400);
     expect(response.body.success).toBe(false);
-    expect(response.body.message).toEqual('Invalid URL params');
+    expect(response.body.message).toEqual('No query content');
   });
-  
-  it('should return status 400 with negative param', async () => {
+
+  it('should return status 400 missing field on query', async () => {
+    const response = await request(app)
+      .get('/pointsOfInterest/byProximity?distance=10&coordinateX=20');
+
+    expect(response.status).toBe(400);
+    expect(response.body.success).toBe(false);
+    expect(response.body.message).toEqual('Invalid query content');
+  });
+
+  it('should return status 400 invalid field on query', async () => {
+    const response = await request(app)
+      .get('/pointsOfInterest/byProximity?distance=10&coordinateX=text&coordinateY=10');
+
+    expect(response.status).toBe(400);
+    expect(response.body.success).toBe(false);
+    expect(response.body.message).toEqual('Invalid query content');
+  });
+
+  it('should return status 400 with negative value on query', async () => {
     const response = await request(app)
       .get('/pointsOfInterest/byProximity?distance=-10&coordinateX=20&coordinateY=10');
 
     expect(response.status).toBe(400);
     expect(response.body.success).toBe(false);
-    expect(response.body.message).toEqual('Negative values are\'t accepted');
+    expect(response.body.message).toEqual("Negative values aren't accepted");
   });
 });
