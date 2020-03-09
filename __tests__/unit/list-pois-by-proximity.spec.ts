fce0504b64f6f761e1cb13bad16a74a2cfb72eea
Gabriel de Freitas Pinheiro <gabrieldefreitaspinheiro@gmail.com>
2020-03-09 03:23:17 -0300

    Fix test of POIs listing by proximity

---
+++ __tests__/unit/list-pois-by-proximity.spec.ts
@@ -0,0 +1,39 @@
+import MongoMock from '../utils/MongoMock';
+
+import ListPOIsByProximityService from '../../src/services/ListPOIsByProximityService';
+
+import POI from '../../src/schemas/PointOfInterest';
+
+import { poisArrayMock } from '../utils/arrays-mock';
+
+describe('List Points of Interest by Proximity', () => {
+  beforeAll(async () => {
+    await MongoMock.connect();
+  });
+
+  afterAll(async () => {
+    await MongoMock.disconnect();
+  });
+
+  beforeEach(async () => {
+    await POI.deleteMany({});
+  });
+
+  it('should be able list interest point by proximity', async () => {
+    await POI.create(poisArrayMock);
+
+    const poisArray =
+      await ListPOIsByProximityService.run({ distance: 10, coordinateX: 20, coordinateY: 10 });
+
+    expect(poisArray).toBeInstanceOf(Array);
+    expect(poisArray.length).toBe(4);
+    expect(poisArray).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining(poisArrayMock[0]),
+        expect.objectContaining(poisArrayMock[2]),
+        expect.objectContaining(poisArrayMock[4]),
+        expect.objectContaining(poisArrayMock[5])
+      ])
+    );
+  });
+});
