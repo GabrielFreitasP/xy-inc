03a446c4704e04bf3eee85b55f2adafa69c42487
Gabriel de Freitas Pinheiro <gabrieldefreitaspinheiro@gmail.com>
2020-03-09 03:27:32 -0300

    Add validation error test on tests of POI creating

--- __tests__/unit/calculate-distance.spec.ts
+++ __tests__/unit/calculate-distance.spec.ts
@@ -1,19 +1,19 @@
 import CalculateDistance, { Point } from '../../src/utils/calculate-distance';
 
 describe('Calculete Distance', () => {
-  it('should be able to calculate the distance between two points', async () => {
+  it('should be able to calculate the distance between two points', () => {
     const point1: Point = { coordX: 0, coordY: 0 };
     const point2: Point = { coordX: 3, coordY: 4 };
-    
+
     const distance = CalculateDistance(point1, point2);
 
     expect(distance).toBe(5);
   });
 
-  it('should be able to calculate the distance between two points with posite result', async () => {
+  it('should be able to calculate the distance between two points with posite result', () => {
     const point1: Point = { coordX: 3, coordY: 4 };
     const point2: Point = { coordX: 0, coordY: 0 };
-    
+
     const distance = CalculateDistance(point1, point2);
 
     expect(distance).toBe(5);
--- __tests__/unit/create-poi.spec.ts
+++ __tests__/unit/create-poi.spec.ts
@@ -1,9 +1,9 @@
 import MongoMock from '../utils/MongoMock';
-import InterestPointData from '../../src/services/structures/PointOfInterestData';
 
 import CreatePOIService from '../../src/services/CreatePOIService';
 
-import InterestPoint from '../../src/schemas/PointOfInterest';
+import POI from '../../src/schemas/PointOfInterest';
+import POIData from '../../src/services/structures/PointOfInterestData';
 
 describe('Create Point of Interest', () => {
   beforeAll(async () => {
@@ -15,24 +15,40 @@ describe('Create Point of Interest', () => {
   });
 
   beforeEach(async () => {
-    await InterestPoint.deleteMany({});
+    await POI.deleteMany({});
   });
 
   it('should be able to create new point of interest', async () => {
-    const interestPoint: InterestPointData = {
+    const poi: POIData = {
       name: 'Lanchonete',
       coordinateX: 27,
       coordinateY: 12
     };
 
-    await CreatePOIService.run(interestPoint);
+    await CreatePOIService.run(poi);
 
-    const interestPointsArray = await InterestPoint.find({});
+    const poisArray = await POI.find({}).lean();
 
-    expect(interestPointsArray).toEqual(
+    expect(poisArray.length).toBe(1);
+    expect(poisArray).toEqual(
       expect.arrayContaining([
-        expect.objectContaining(interestPoint)
+        expect.objectContaining(poi)
       ])
     );
+  });
+
+  it('should throw a validation error', async () => {
+    const poi: POIData = {
+      name: 'Lanchonete',
+      coordinateX: 27,
+      coordinateY: -12
+    };
+
+    try {
+      await CreatePOIService.run(poi);
+    } catch (err) {
+      expect(err.name).toEqual('ValidationError');
+      expect(err._message).toEqual('PointOfInterest validation failed');
+    }
   });
 });
